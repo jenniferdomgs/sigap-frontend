34c59b40ea7407c7831ca3e38db5907ad8494e1a
Jennifer <99359081+jenniferdomgs@users.noreply.github.com>
2024-10-01 11:56:40 -0300

    perfil - ok (static, por enquanto)

--- front/src/views/perfil.js
+++ front/src/views/perfil.js
@@ -1,33 +1,79 @@
 import React from 'react';
-import '../assets/css/perfil.css'
-import '../assets/fonts/fontawesome/css/all.min.css'
+import '../assets/css/perfil.css';
+import '../assets/fonts/fontawesome/css/all.min.css';
 
 const Perfil = () => {
     return (
         <div className='container-prop'>
+         
             <div className='header-table'>
                 <h4>Proprietário da Arma</h4>
-                <i class="fa-solid fa-pen-to-square"></i>
+                <i className="fa-solid fa-pen-to-square"></i>
             </div>
 
             <div className='dadosP'>
-                <div>
-                    <p>Nome:</p>
-                    <p>CPF:</p>
-                    <p>Telefone:</p>
+                <div className='coluna'>
+                    <p><strong>Nome:</strong></p>
+                    <p><strong>CPF:</strong></p>
+                    <p><strong>Telefone:</strong></p>
                 </div>
-                    
-                <div>
-                    <p>Identidade:</p>
-                    <p>Email:</p>
+                <div className='coluna'>
+                    <p><strong>Identidade:</strong></p>
+                    <p><strong>Email:</strong></p>
                 </div>
+                <div className='coluna perfil-section'>
+                    <img className='perfil' src='path_to_profile_picture' alt='foto 3x4' />
+                    <p className='status'><strong>Status:</strong> Ativo</p>
+                </div>
+            </div>
 
-                <div>
-                    <img className='perfil' alt='foto 3x4'></img>
+            
+            <div className='header-table'>
+                <h4>Armas Cadastradas</h4>
+            </div>
+
+       
+            <div className='armas-cadastradas'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nº Série</th>
+                            <th>Espécie</th>
+                            <th>Calibre</th>
+                            <th>Marca</th>
+                            <th>Modelo</th>
+                            <th>Nº Sigma</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>XXXX</td>
+                            <td>XXXX</td>
+                            <td>XXXX</td>
+                            <td>XXXX</td>
+                            <td>XXXX</td>
+                            <td>XXXX</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+               
+                <div className='acao-btn'>
+                    <button>
+                        <i className="fa-solid fa-hourglass-half"></i>
+                        Aguardando Autorização
+                    </button>
+                    <button>
+                        <i className="fa-solid fa-eye"></i>
+                        Visualizar Histórico
+                    </button>
+                    <button>
+                        <i className="fa-solid fa-file"></i>
+                        Solicitar CRAF
+                    </button>
                 </div>
             </div>
         </div>
-        
     );
 };
 
