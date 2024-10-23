3fa3153599fc380f302536af577eb29f5fa9f3bc
Jennifer <99359081+jenniferdomgs@users.noreply.github.com>
2024-10-23 11:06:03 -0300

    add jwttoken - perfil

--- front/src/views/perfil.js
+++ front/src/views/perfil.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../assets/css/perfil.css';
 import '../assets/fonts/fontawesome/css/all.min.css';
 import axios from 'axios';
 
-
 const Perfil = () => {
-
+    const [dados, setDados] = useState({});
+    const token = localStorage.getItem('jwtToken'); 
+    
     useEffect(() => {
-        axios.get('http://localhost:3000/sigap/api/dadosPolicial')
-            .then(response => setDados(response.data))
-            .catch(error => console.error('Erro ao buscar os dados', error));
-    }, []);
+        axios.get('http://localhost:3000/sigap/api/dadosPolicial', {
+            headers: {
+                Authorization: `Bearer ${token}` 
+            }
+        })
+        .then(response => setDados(response.data))
+        .catch(error => console.error('Erro ao buscar os dados', error));
+    }, [token]);
 
     return (
         <div className='container-prop'>
@@ -22,26 +27,24 @@ const Perfil = () => {
 
             <div className='dadosP'>
                 <div className='coluna'>
-                    <p><strong>Nome:</strong>{Dados.st_nome}</p>
-                    <p><strong>CPF:</strong>{Dados.st_cpf}</p>
-                    <p><strong>Telefone:</strong>{Dados.st_telefonecelular}</p>
+                    <p><strong>Nome:</strong> {dados.st_nome}</p>
+                    <p><strong>CPF:</strong> {dados.st_cpf}</p>
+                    <p><strong>Telefone:</strong> {dados.st_telefonecelular}</p>
                 </div>
                 <div className='coluna'>
-                    <p><strong>Posto/Graduação:</strong>{Dados.st_postograduacao}</p>
-                    <p><strong>Email:</strong>{Dados.st_email}</p>
+                    <p><strong>Posto/Graduação:</strong> {dados.st_postograduacao}</p>
+                    <p><strong>Email:</strong> {dados.st_email}</p>
                 </div>
                 <div className='coluna perfil-section'>
                     <img className='perfil' src='path_to_profile_picture' alt='foto 3x4' />
-                    <p className='status'><strong>Status:</strong>{Dados.st_status}</p>
+                    <p className='status'><strong>Status:</strong> {dados.st_status}</p>
                 </div>
             </div>
 
-            
             <div className='header-table'>
                 <h4>Armas Cadastradas</h4>
             </div>
 
-       
             <div className='armas-cadastradas'>
                 <table>
                     <thead>
@@ -66,7 +69,6 @@ const Perfil = () => {
                     </tbody>
                 </table>
 
-               
                 <div className='acao-btn'>
                     <button>
                         <i className="fa-solid fa-hourglass-half"></i>
@@ -87,4 +89,3 @@ const Perfil = () => {
 };
 
 export default Perfil;
-
